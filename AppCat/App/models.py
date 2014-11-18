491cef4e18af0e55024a73b251e146b1d59e3a7f
Henry Ni <nihy@miamioh.edu>
2014-11-18 16:31:26 -0500

    making sure all files are up to date on new branch

--- AppCat/App/models.py
+++ AppCat/App/models.py
@@ -8,4 +8,10 @@ class app(models.Model):
     link = models.URLField()
 #    pic = models.FileField(upload_to='apps')
     rating = models.DecimalField(max_digits=3,decimal_places=2)
+
+ class requests(modesl.Model):
+ 	requestor = models.CharField(max_length = 35)
+ 	app_name = models.CharField(max_length = 100)
+ 	description = models.TextField(default = 'Why should we add this to the catalog?')
+ 	link = models.URLField();
     
